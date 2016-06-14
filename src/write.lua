9aad2ef46a4252ce9b90afbee97e4db1a0de5b8c
Timothy D <timothydowns@gmail.com>
2016-06-14 18:36:39 +1000

    add auth to write route

--- src/write.lua
+++ src/write.lua
@@ -6,12 +6,14 @@ function write.route(redis, arg1, arg2, method)
 
   -- arg1 is orgname
   -- arg2 is dashboard
+  local headers = ngx.req.get_headers()
+  local request_password = headers['key']
   if arg1 and arg2 and method == "POST" then
     ngx.req.read_body()
     local body_data = ngx.req.get_body_data()
     if type(body_data) == "string" and string.len(body_data) > 0 then
       for ln in body_data:gmatch("[^\r\n]+") do
-        record.log(redis, arg1, arg2, ln)
+        record.log(redis, arg1, arg2, request_password, ln)
       end
     end
     return true
