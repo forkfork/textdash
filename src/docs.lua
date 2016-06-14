af7c43807d148b7831f92024e2c18407a4e4eab5
Timothy D <timothydowns@gmail.com>
2016-06-14 23:39:55 +1000

    delete dead code

--- src/docs.lua
+++ src/docs.lua
@@ -11,24 +11,4 @@ function docs.route(redis, arg1, arg2, method)
   end
 end
 
-
-function docs.docs()
-  ngx.say([[textdash - no-install remote log watching
-
-curl -4 https://textdash.xyz/register/me@example.com
-
-[api key emailed to me@example.com]
-
-curl -4 -X POST -d "im log data" https://textdash.xyz/yourapikey/applicationxyz
-curl -4 -X POST -d "im log data also" https://textdash.xyz/yourapikey/applicationxyz
-curl -4 https://textdash.xyz/yourapikey/applicationxyz ==> replies "im log data" "im log data also"
-
-Features:
-
-* read a dashboard with header "accept: text/event-stream" and updates will be streamed to you
-* as many dashboards as you would like
-* logs roll around on a particular dashboard
-]])
-end
-
 return docs
